763522de80a95626857cb03314f97e6c9884db77
jkarmanski <jakub.karmanski@softiq.pl>
2023-01-21 23:35:36 +0100

    nowy layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,40 +27,62 @@ export class AppComponent implements OnInit {
   wartosc: any;
 
   PROJEKTY: Projekt[] = [
-    { id: 0, nazwa: 'SRB, OSAT, PEF, UZP', opiekun: 'Kuba Plachta' },
+    { id: 0, nazwa: 'SRB, OSAT, PEF, GIS', opiekun: 'Kuba Plachta' },
     { id: 1, nazwa: 'PIU-EMP@TIA', opiekun: 'Kuba Karmański' },
     { id: 2, nazwa: 'CSIZS, ZDM, UMK', opiekun: 'Przemek Kubisa' },
     { id: 3, nazwa: 'CLUIID, ALK(ePromotor)', opiekun: 'Zbyszek Szot' },
     { id: 4, nazwa: 'WORTAL PSZ', opiekun: 'Rafał Grzelec' },
-    { id: 5, nazwa: 'KDR', opiekun: 'Staszek Sobas' },
+    { id: 5, nazwa: 'KDR, ESSI, MOW, MZT, MZS', opiekun: 'Staszek Sobas' },
     { id: 6, nazwa: 'RZ', opiekun: 'Kasia Frank' },
-    { id: 5, nazwa: 'CITY OF REYKJAVIK, BROKER SI PSZ', opiekun: 'Michał Tęcza' }
+    { id: 7, nazwa: 'CITY OF REYKJAVIK, BROKER SI PSZ', opiekun: 'Michał Tęcza' }
   ]
 
   uzyte: number[] = [];
+  index: number = 0;
 
   ngOnInit(): void {
-    this.wartosc = new Projekt(0, '', '');
+    this.wartosc = new Projekt(0, 'NA DRESZCZYK EMOCJI', 'PRZYGOTUJCIE SIĘ');
   }
 
-  losujWartosc() {
-    let number = Math.floor(Math.random() * this.PROJEKTY.length);
-    if (this.uzyte.length == this.PROJEKTY.length) {
-      this.wartosc = new Projekt(0, 'WSZYSTKO OMÓWIONE', 'ZAPRASZAMY ZA TYDZIEŃ');
+  losujWartosci() {
+    for (let index = 0; index < this.PROJEKTY.length; index++) {
+      let number = Math.floor(Math.random() * this.PROJEKTY.length);
+      while (this.uzyte.includes(number)) {
+        number = Math.floor(Math.random() * this.PROJEKTY.length);
+      }
+      this.uzyte.push(number);
+    }
+    this.PROJEKTY.push(new Projekt(this.PROJEKTY.length-1, 'WSZYSTKO OMÓWIONE', 'ZAPRASZAMY ZA TYDZIEŃ'));
+    this.uzyte.push(this.PROJEKTY.length-1);
+  }
+
+  prevValue() {
+    if(this.index === 0){
       return;
     }
-    while (this.uzyte.includes(number)) {
-      number = Math.floor(Math.random() * this.PROJEKTY.length);
+    this.index--;
+    this.wartosc = this.PROJEKTY[this.uzyte[this.index]];
+  }
+
+  nextValue() {
+    if (this.uzyte.length == 0) {
+      this.losujWartosci();
+      this.wartosc = this.PROJEKTY[this.uzyte[this.index]];
     }
-
-    this.wartosc = this.PROJEKTY[number];
-    this.uzyte.push(number);
-
+    else if(this.index === this.uzyte.length-1){
+      this.wartosc = this.PROJEKTY[this.uzyte[this.index]];
+      return;
+    }
+    else{
+      this.index++;
+      this.wartosc = this.PROJEKTY[this.uzyte[this.index]];
+    }
   }
 
   reset() {
     this.uzyte = [];
     this.wartosc = new Projekt(0, '', '');
+    this.index = 0;
   }
 
 
