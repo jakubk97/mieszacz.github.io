23ad44f8533679d5976ca9197be9926ae04cb51e
jkarmanski <jakub.karmanski@softiq.pl>
2023-05-26 09:48:02 +0200

    aktualizacja projektow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent implements OnInit {
     { id: 3, nazwa: 'WORTAL PSZ', opiekun: 'Rafał Grzelec', media: null },
     { id: 4, nazwa: 'KDR, EESSI', opiekun: 'Staszek Sobas', media: null },
     { id: 5, nazwa: 'RZ', opiekun: 'Kasia Frank', media: null },
-    { id: 6, nazwa: 'CITY OF REYKJAVIK', opiekun: 'Michał Tęcza', media: null },
+    { id: 6, nazwa: 'CITY OF REYKJAVIK, BROKER', opiekun: 'Michał Tęcza', media: null },
     { id: 7, nazwa: 'SOF', opiekun: 'Angelika Świacka', media: null },
     { id: 8, nazwa: 'SRB', opiekun: 'Kamil Błoński', media: null },
     { id: 9, nazwa: 'OSAT', opiekun: 'Kamil Susek', media: null },
