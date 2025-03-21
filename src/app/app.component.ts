3304893904ee7457dd68c0fc68bbf78b01f83fb9
Jakub Karmański <jakub.karmanski@softiq.pl>
2025-03-21 09:11:31 +0100

    zmiana proj Konrad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,9 +37,9 @@ export class AppComponent implements OnInit {
     { id: 5, nazwa: 'BROKER', opiekun: 'Przemek Chlebek', media: null },
     { id: 6, nazwa: 'SOF', opiekun: 'Angelika Świacka', media: null },
     { id: 7, nazwa: 'ZDM', opiekun: 'Michał Pocheć', media: null },
-    { id: 8, nazwa: 'UMK, SIO', opiekun: 'Piotr Adamczyk', media: null },
+    { id: 8, nazwa: 'SIO', opiekun: 'Piotr Adamczyk', media: null },
     { id: 9, nazwa: 'NFZ CRM', opiekun: 'Sebastian Nawrocki', media: null },
-    { id: 10, nazwa: 'PFRON WSO2', opiekun: 'Konrad Balbuza', media: null },
+    { id: 10, nazwa: 'PFRON WSO2, UMK', opiekun: 'Konrad Balbuza', media: null },
     { id: 11, nazwa: 'projekty AI', opiekun: 'Adam Marszowski', media: null }
   ]
 
